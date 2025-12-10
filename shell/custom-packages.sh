911de8bd8dc788dc268c1678eb49a520628ce435
Masaaki <147675004+yndzm@users.noreply.github.com>
2025-12-11 00:34:01 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -31,7 +31,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-homeproxy-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-kucat"
 # 进阶设置 by sirpdboy 
 # 当luci-app-kucat-config插件开启时 需排除冲突项 luci-app-argon-config和luci-i18n-argon-config-zh-cn 减号代表排除
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-kucat-config luci-kucat-config-zh-cn -luci-app-argon-config -luci-i18n-argon-config-zh-cn"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-kucat-config luci-kucat-config-zh-cn -luci-app-argon-config -luci-i18n-argon-config-zh-cn"
 # 网络测速 by sirpdboy 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netspeedtest luci-i18n-netspeedtest-zh-cn"
 # MosDNS
